e1e9d16c4e86ecc30c6dd44ffc5870f74f56d0a2
amfchef <jakobtj@hotmail.com>
2021-04-25 11:24:09 +0200

    added functions

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,7 @@ class TestCalc:
     def test_sub(self):
         assert 10 == Calc.sub(20, 10)
         assert 100 == Calc.sub(100, 0)
+
+    def test_multi(self):
+        assert 100 == Calc.multi(10,10)
+        assert 50 == Calc.multi(5,10)
